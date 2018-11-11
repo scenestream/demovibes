ebde02ac8b9c4e09531bcc38c51ba64af1df35d7
phlubby <phlubby@gmail.com>
2018-11-11 03:36:19 +0100

    Minor cleanups

--- demovibes/prelisten.py
+++ demovibes/prelisten.py
@@ -21,38 +21,48 @@ class Prelisten(object):
         return os.path.join(self.prelisten_dir, str(self.id) + '.mp3')
 
     def ensure_prelisten(self):
-        filename, file_ext = os.path.splitext(self.file_path)
-        #  Prelisten files will be stored to a prelisten dir; we can use a cron job to peridically purge it
-        wav_path = self.prelisten_dir + str(self.id) + ".wav"
+        # Prelisten files will be stored in a prelisten dir; we can use a
+        # cron job to periodically purge it.
         mp3_path = self.path()
         flag_path = self.prelisten_dir + str(self.id) + ".enc"
-        # check if the prelisten file already exists, or is in progress
+
+        # Check if the prelisten file already exists, or is in progress.
         if os.path.isfile(mp3_path) or os.path.isfile(flag_path):
-          return
-        # if the file is already an mp3, make a symlink instead
+            return
+
+        unused_filename, file_ext = os.path.splitext(self.file_path)
+        # If the file is already an mp3, make a symlink instead.
         if file_ext == '.mp3':
             os.symlink(self.file_path, mp3_path)
             return
-        # otherwise use dscan and lame to create one
-        # first create a 'flag' file that says encoding is in progress
-        # the view checks for this to avoid launching multiple encoding threads for the same file
+
+        # Otherwise use dscan and lame to create one.
+        # First create a 'flag' file that says encoding is in progress.
         open(flag_path, 'a').close()
+
         dscan = getattr(settings, 'DEMOSAUCE_SCAN', False)
         lame = getattr(settings, 'LAME', "/usr/bin/lame")
+        wav_path = self.prelisten_dir + str(self.id) + ".wav"
+
         ret = subprocess.call([dscan, "-o", wav_path, self.file_path])
         if ret != 0:
-            log.debug("Could not dscan %s to %s: %s" % (self.file_path, wav_path, "some reason"))
+            log.debug("Could not dscan %s to %s: %s"
+                      % (self.file_path, wav_path, "some reason"))
             return
+
         ret = subprocess.call([lame, "-S", wav_path, mp3_path])
         if ret != 0:
-            log.debug("Could not lame %s to %s: %s" % (wav_path, mp3_path, "some reason"))
+            log.debug("Could not lame %s to %s: %s"
+                      % (wav_path, mp3_path, "some reason"))
             return
+
         os.unlink(wav_path)
         os.unlink(flag_path)
-        log.debug("Created prelisten file for %s at %s." % (self.file_path, mp3_path))
+
+        log.debug("Created prelisten file for %s at %s."
+                  % (self.file_path, mp3_path))
 
     def has_prelisten(self):
-        # This is a hack so that the templates can avoid showing a dead player if the prelisten file hasn't been generated yet
-        if os.path.isfile(self.path()):
-            return True
-        return False
+        # This is a hack so that the templates can avoid showing a dead player
+        # if the prelisten file hasn't been generated yet
+        return os.path.isfile(self.path())
