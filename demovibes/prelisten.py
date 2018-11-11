859b13ae6f0f2dffa602f3a0ba60ea4a478a6209
phlubby <phlubby@gmail.com>
2018-11-11 02:50:39 +0100

    Refactor prelisten code into prelisten.py

---
+++ demovibes/prelisten.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+from django.conf import settings
+import logging
+
+log = logging.getLogger("dv.prelisten")
+
+
+class Prelisten(object):
+    def __init__(self, file_path, song_id):
+        self.file_path = file_path
+        self.id = song_id
+
+    def ensure_prelisten(self):
+        filename, file_ext = os.path.splitext(self.file_path)
+        #  Prelisten files will be stored to a prelisten dir; we can use a cron job to peridically purge it
+        prelisten_dir = getattr(settings, 'MEDIA_ROOT', False) + "media/music/prelisten/"
+        wav_path = prelisten_dir + str(self.id) + ".wav"
+        mp3_path = prelisten_dir + str(self.id) + ".mp3"
+        flag_path = prelisten_dir + str(self.id) + ".enc"
+        # check if the prelisten file already exists, or is in progress
+        if os.path.isfile(mp3_path) or os.path.isfile(flag_path):
+          return
+        # if the file is already an mp3, make a symlink instead
+        if file_ext == '.mp3':
+            os.symlink(self.file_path, prelisten_dir + str(self.id) + ".mp3")
+            return
+        # otherwise use dscan and lame to create one
+        # first create a 'flag' file that says encoding is in progress
+        # the view checks for this to avoid launching multiple encoding threads for the same file
+        open(flag_path, 'a').close()
+        dscan = getattr(settings, 'DEMOSAUCE_SCAN', False)
+        lame = getattr(settings, 'LAME', "/usr/bin/lame")
+        ret = subprocess.call([dscan, "-o", wav_path, self.file_path])
+        if ret != 0:
+            log.debug("Could not dscan %s to %s: %s" % (self.file_path, wav_path, "some reason"))
+            return
+        ret = subprocess.call([lame, "-S", wav_path, mp3_path])
+        if ret != 0:
+            log.debug("Could not lame %s to %s: %s" % (wav_path, mp3_path, "some reason"))
+            return
+        os.unlink(wav_path)
+        os.unlink(flag_path)
+        log.debug("Created prelisten file for %s at %s." % (self.file_path, mp3_path))
+
+    def has_prelisten(self):
+        # This is a hack so that the templates can avoid showing a dead player if the prelisten file hasn't been generated yet
+        prelisten_dir = getattr(settings, 'MEDIA_ROOT', False) + "media/music/prelisten/"
+        if os.path.isfile(prelisten_dir + str(self.id) + ".mp3"):
+            return True
+        return False
--- demovibes/webview/models.py
+++ demovibes/webview/models.py
@@ -13,6 +13,8 @@ from django.core.mail import EmailMessage
 #from django.core.urlresolvers import reverse
 import dscan
 import logging
+import prelisten
+
 import xml.dom.minidom, urllib # Needed for XML processing
 from django.core.cache import cache
 from django.template.defaultfilters import striptags
@@ -1086,43 +1088,16 @@ class Song(models.Model):
         if self.downloadable_by(user):
            return protected_downloads.get_song_url(self, user)
         return False
+
+    def prelisten(self):
+        return prelisten.Prelisten(self.file.path, self.id)
+
     def ensure_prelisten(self):
-        filename, file_ext = os.path.splitext(self.file.path)
-        #  Prelisten files will be stored to a prelisten dir; we can use a cron job to peridically purge it
-        prelisten_dir = getattr(settings, 'MEDIA_ROOT', False) + "media/music/prelisten/"
-        wav_path = prelisten_dir + str(self.id) + ".wav"
-        mp3_path = prelisten_dir + str(self.id) + ".mp3"
-        flag_path = prelisten_dir + str(self.id) + ".enc"
-        # check if the prelisten file already exists, or is in progress
-        if os.path.isfile(mp3_path) or os.path.isfile(flag_path):
-          return
-        # if the file is already an mp3, make a symlink instead
-        if file_ext == '.mp3':
-            os.symlink(self.file.path, prelisten_dir + str(self.id) + ".mp3")
-            return
-        # otherwise use dscan and lame to create one
-        # first create a 'flag' file that says encoding is in progress
-        # the view checks for this to avoid launching multiple encoding threads for the same file
-        open(flag_path, 'a').close()
-        dscan = getattr(settings, 'DEMOSAUCE_SCAN', False)
-        lame = getattr(settings, 'LAME', "/usr/bin/lame")
-        ret = subprocess.call([dscan, "-o", wav_path, self.file.path])
-        if ret != 0:
-            log.debug("Could not dscan %s to %s: %s" % (self.file.path, wav_path, "some reason"))
-            return
-        ret = subprocess.call([lame, "-S", wav_path, mp3_path])
-        if ret != 0:
-            log.debug("Could not lame %s to %s: %s" % (wav_path, mp3_path, "some reason"))
-            return
-        os.unlink(wav_path)
-        os.unlink(flag_path)
-        log.debug("Created prelisten file for %s at %s." % (self.file.path, mp3_path))
+        return self.prelisten().ensure_prelisten()
+
     def has_prelisten(self):
-        # This is a hack so that the templates can avoid showing a dead player if the prelisten file hasn't been generated yet
-        prelisten_dir = getattr(settings, 'MEDIA_ROOT', False) + "media/music/prelisten/"
-        if os.path.isfile(prelisten_dir + str(self.id) + ".mp3"):
-            return True
-        return False
+        return self.prelisten().has_prelisten()
+
     def has_video(self):
         return self.get_metadata().ytvidid
 
