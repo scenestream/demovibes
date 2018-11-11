bc0fdcd729074464a8c57ba40faf08515aaa4853
phlubby <phlubby@gmail.com>
2018-11-11 03:26:40 +0100

    Refactor prelisten_dir and mp3_path

--- demovibes/prelisten.py
+++ demovibes/prelisten.py
@@ -13,20 +13,25 @@ class Prelisten(object):
     def __init__(self, file_path, song_id):
         self.file_path = file_path
         self.id = song_id
+        self.prelisten_dir = \
+            os.path.join(getattr(settings, 'MEDIA_ROOT', False)
+                         + 'media/music/prelisten/')
+
+    def path(self):
+        return os.path.join(self.prelisten_dir, str(self.id) + '.mp3')
 
     def ensure_prelisten(self):
         filename, file_ext = os.path.splitext(self.file_path)
         #  Prelisten files will be stored to a prelisten dir; we can use a cron job to peridically purge it
-        prelisten_dir = getattr(settings, 'MEDIA_ROOT', False) + "media/music/prelisten/"
-        wav_path = prelisten_dir + str(self.id) + ".wav"
-        mp3_path = prelisten_dir + str(self.id) + ".mp3"
-        flag_path = prelisten_dir + str(self.id) + ".enc"
+        wav_path = self.prelisten_dir + str(self.id) + ".wav"
+        mp3_path = self.path()
+        flag_path = self.prelisten_dir + str(self.id) + ".enc"
         # check if the prelisten file already exists, or is in progress
         if os.path.isfile(mp3_path) or os.path.isfile(flag_path):
           return
         # if the file is already an mp3, make a symlink instead
         if file_ext == '.mp3':
-            os.symlink(self.file_path, prelisten_dir + str(self.id) + ".mp3")
+            os.symlink(self.file_path, mp3_path)
             return
         # otherwise use dscan and lame to create one
         # first create a 'flag' file that says encoding is in progress
@@ -48,7 +53,6 @@ class Prelisten(object):
 
     def has_prelisten(self):
         # This is a hack so that the templates can avoid showing a dead player if the prelisten file hasn't been generated yet
-        prelisten_dir = getattr(settings, 'MEDIA_ROOT', False) + "media/music/prelisten/"
-        if os.path.isfile(prelisten_dir + str(self.id) + ".mp3"):
+        if os.path.isfile(self.path()):
             return True
         return False
